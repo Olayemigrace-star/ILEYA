601cc666f85723f7de4fee95286b82d7d3a4a5a8
Grace Olayemi <olayemigrace29@gmail.com>
2026-06-01 12:47:05 +0100

    Task

--- DAY3/CheckOut.java
+++ DAY3/CheckOut.java
@@ -14,7 +14,7 @@ public class CheckOut{
         ArrayList<Integer> productUnit = new ArrayList<>();
         ArrayList<Double> costPrice = new ArrayList<>();
                
-        while(!decision.equals("no")){
+        while(true){
             
             System.out.println("What did the user buy: ");
             String product = input.nextLine();
@@ -30,6 +30,8 @@ public class CheckOut{
             input.nextLine();
             System.out.println("Add more items: ");
             decision = input.nextLine();
+            
+            if(decision.equals("no")) break;
             
         }
         
@@ -60,51 +62,5 @@ public class CheckOut{
     double vat = (17.50/100.0) * total;
     double billTotal = total - discount + vat;    
 
-    System.out.printf("%-29s%-16s%.2f%n","","Sub Total:",total);
-    System.out.printf("%-29s%-16s%.2f%n","","Discount:",discount);
-    System.out.printf("%-29s%-16s%.2f%n","","VAT @ 17.50%:",vat);
-    System.out.printf("  =======================================================================%n");
-    System.out.printf("%-29s%-16s%.2f%n","","Bill Total:",billTotal);
-    System.out.printf("  =======================================================================%n");
-    System.out.printf("%-4sTHIS IS NOT A RECEIPT KINDLY PAY %.2f%n","",billTotal);
-    System.out.printf("  =======================================================================%n%n");
-
-    System.out.println("How much did the customer give to you?");
-    double amountPayed = input.nextDouble();
-    System.out.println();
-
-
-    System.out.printf("SEMICOLON STORES%nMAIN BRANCH%nLOCATION: 312, HERBERT MACAULAY WAY, SABO YABO, LAGOS.%nTEL: 03293828343%nDate : 18-Dec-22 8:48:11 pm%nCashier: %s %nCustomer's Name: %s%n%n", cashiersName,customer);
-    System.out.printf("  =======================================================================%n");
-    System.out.printf("%-15s%-14s%-14s%-14s%-14s%n","","ITEM", "QTY", "PRICE", "TOTAL(NGN)");
-    System.out.printf("  -----------------------------------------------------------------------%n");
-    for(int counter = 0; counter < productName.size(); counter++){
-
-        System.out.printf("%-15s%-14s%-14d%-14.2f%-14.2f%n","", productName.get(counter), productUnit.get(counter), costPrice.get(counter), costPrice.get(counter) * productUnit.get(counter));
-
     }
-    System.out.printf("%n  -----------------------------------------------------------------------%n");
-    //double total = 0;
-    for(int counter = 0; counter < productName.size(); counter++){
-
-        total = total + costPrice.get(counter) * productUnit.get(counter);
-
     }
-    discount = (discount/100.0) * total;
-    double balance = amountPayed - billTotal;
-
-    System.out.printf("%-29s%-16s%.2f%n","","Sub Total:",total);
-    System.out.printf("%-29s%-16s%.2f%n","","Discount:",discount);
-    System.out.printf("%-29s%-16s%.2f%n","","VAT @ 17.50%:",vat);
-    System.out.printf("  =======================================================================%n");
-    System.out.printf("%-29s%-16s%.2f%n","","Bill Total:",billTotal);
-    System.out.printf("%-29s%-16s%.2f%n","","Amount Paid:",amountPayed);
-    System.out.printf("%-29s%-16s%.2f%n","","Balance:",balance);
-    System.out.printf("  =======================================================================%n");
-    System.out.printf("%-15sTHANK YOU FOR YOUR PATRONAGE%n","");
-    System.out.printf("  =======================================================================%n%n");
-
-
-    }
-    
-}
